7b1793bc0f03b1ba3adbd1e3952ea6fec90cbcce
Felix Barten <felixbarten@users.noreply.github.com>
2018-11-16 20:55:52 +0100

    Added votes percentage calculation on dashboard.

--- biznetwork.js
+++ biznetwork.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Biz.Network ARK per day
 // @namespace    https://classicdelegate.biz
-// @version      1.0
-// @description  Displays ARK gained per day on the biz.network delegate dashboard. Future updates to dashboard may (irrevocably) break this script.
+// @version      1.1
+// @description  Displays ARK gained per day on the biz.network delegate dashboard. Future updates to the delegate's dashboard may (irrevocably) break this script.
 // @author       Felix Barten
 // @match        https://classicdelegate.biz/dashboard
-// @match 		   https://privatedelegate.biz/dashboard
+// @match 		 https://privatedelegate.biz/dashboard
 // @grant        none
-// @require  	   http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require  	   https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require  	 http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require  	 https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
@@ -24,13 +24,23 @@ function addJQuery(callback) {
 }
 
 var main = function () {
-	function update() {
+    function run() {
+        updateARKPerDay();
+        percentageOfVotes();
+    }
+
+	function updateARKPerDay() {
 		console.log("Calculating ARK gain per day");
 		var arkPerDay = arkDue() / returnDays();
 		console.log(arkPerDay, " ARK per day this payment period");
-		jQ("div#payoutStats").append("<br> <b> ARK per day: </b>");
-		jQ("div#payoutStats").append(arkPerDay);
+        if(!isNaN(arkPerDay)) {
+            console.log(arkPerDay, " ARK per day this payment period");
+		    jQ("div#payoutStats").append("<br> <b> ARK per day: </b>");
+            jQ("div#payoutStats").append(arkPerDay);
+        }
 	}
+	
+    // Returns the days passed after the payout date. Known issue: if in a different timezone than delegate may be inaccurate.
 	function returnDays() {
 		var str = jQ("div#payoutStats").text()
 		var regex = /\w\w\w\s\w\w\w\s\d\d?\s\d\d\d\d/;
@@ -50,7 +60,22 @@ var main = function () {
         }
         return matches[0];
 	}
-	update();
+
+    // calculates the percentage of votes your balance carries.
+    function percentageOfVotes() {
+		var voterAmount = jQ("div#largeStats div:nth-child(2) span.statValue").text();
+		voterAmount = voterAmount.replace(/,/g, "");
+        var balanceAmount = jQ("div#balanceStats").text();
+
+        // match balance from the string.
+        var matches = balanceAmount.match(/[0-9]*\.[0-9]*/g);
+        var percentage = parseFloat(matches[0]) / parseFloat(voterAmount) * parseFloat(100.0);
+
+        jQ("div#balanceStats").append("<br> <b> Votes percentage: </b>");
+		jQ("div#balanceStats").append(percentage.toFixed(5) + "%");
+    }
+
+	run();
 };
 
 // Wait for page add data.
